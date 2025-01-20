413d84fafc42e1c756569cfd546100bb9e78f94c
Winstonshih <xxinfernobladexx@gmail.com>
2025-01-20 18:56:32 -0500

    Final commit

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
+"""
+This module uses emotion detection API. It creates a Flask application that
+processes user text and returns values of detected emotions.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app=Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def emo_detector():
-    # Retrieve the text to analyze from the request arguments
+    """
+    Retrieves text after a request, passes text to emotion detector, and
+    returns a formatted response of the emotion values and dominant emotion.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
-    # Extract the label and score from the response
     joy = response['joy']
     anger = response['anger']
     fear=response['fear']
@@ -16,10 +21,14 @@ def emo_detector():
     dominant=response['dominant_emotion']
     if dominant is None:
         return "Invalid text! Please try again!"
-    else:
-        return "For the given statement, the system reponse is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant)
+    return (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant}."
+    )
 @app.route("/")
 def render_index_page():
+    """Renders index HTML page of emotion detector website."""
     return render_template('index.html')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
